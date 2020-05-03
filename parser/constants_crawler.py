3cd96da37e149ea0737bdae69a6af078a3dfe6d9
mtosch <maximilian.tosch@rub.de>
2020-05-03 16:18:44 +0200

    renaming and regex comment formatting

--- parser/constants_crawler.py
+++ parser/constants_crawler.py
@@ -2,13 +2,13 @@
 import os
 from os import path
 import re
+
 # import dashtable
-import datetime
 
 
 def remove_table(file_str):
-    """ 
-    Removes the table from the file content string if one exists 
+    """
+    Removes the table from the file content string if one exists
     :param file_str:    the whole file content as a string
     :type file_str:     str
     :return:            the new file content as a string
@@ -21,7 +21,12 @@ def remove_table(file_str):
     # .*        any character zero or more times
     # ---\+     three dashes followed by plus character. Even the smallest possible dashtable always satisfies this condition
     # \n{1}     one line breaks that was previously added. remove itm to not add additional line breaks every time
-    return re.sub(r'(-{2,}\s*\+)(.*)(---\+)\n{1}', '', file_str, flags=re.MULTILINE|re.DOTALL)
+    return re.sub(
+        r"(-{2,}\s*\+)(.*)(---\+)\n{1}",
+        "",
+        file_str,
+        flags=re.MULTILINE | re.DOTALL,
+    )
 
 
 def add_table(file_str, dash_table):
@@ -30,10 +35,11 @@ def add_table(file_str, dash_table):
     :param file_str:    the whole file content as a string
     :type file_str:     str
     :param dash_table:  the dash table
-    :type dash_table:   str 
+    :type dash_table:   str
     :return:            the new file content as a string
     :rtype:             str
     """
+    re.match()
     # [^- ]+        match anything that is NOT a dash or a whitespace one or more times
     # (\s*entity)   zero or more whitespaces followed by "entity". capture group. idx = 1
     # \s+           one or more whitespaces
@@ -41,10 +47,10 @@ def add_table(file_str, dash_table):
     # \s+           one or more whitespaces
     # is            "is"
     # \s*?          zero or more whitespaces. lazy evaluation
-    m = re.search(r'[^- ]+(\s*entity)\s+.+\s+is\s*?', file_str, re.IGNORECASE)
-    pos = m.start(1) # start of capture group with idx = 1
+    m = re.search(r"[^- ]+(\s*entity)\s+.+\s+is\s*?", file_str, re.IGNORECASE)
+    pos = m.start(1)  # start of capture group with idx = 1
     # TODO catch error if nothing is found
-    return file_str[:pos] + dash_table + "\n" +  file_str[pos:]
+    return file_str[:pos] + dash_table + "\n" + file_str[pos:]
 
 
 def get_constants_from_pkg(pkg_file_path):
@@ -55,30 +61,36 @@ def get_constants_from_pkg(pkg_file_path):
     :return:                a tuple of str lists [name, type, default value] for every constant
     :rtype:                 TODO
     """
-    with open(pkg_file_path, 'r') as f:
+    with open(pkg_file_path, "r") as f:
         constants_file_str = f.read()
-    ## remove all VHDL comments
+    # remove all VHDL comments
     # (\s*--)     any capture group starting with zero or more whitespaces followed by two dashes
     # .*          any character
     # \n?         line break at the end. lazy evaluation
-    constants_file_str = re.sub(r'(\s*--).*\n?', '', constants_file_str, flags=re.MULTILINE)
+    constants_file_str = re.sub(
+        r"(\s*--).*\n?", "", constants_file_str, flags=re.MULTILINE
+    )
 
-    ## substitute all tabs, newlines and other whitespaces
+    # substitute all tabs, newlines and other whitespaces
     # the .strip() removes any leading and trailing whitespaces
     # \s+         one ore more whitespaces
-    constants_file_str = re.sub(r'\s+', ' ', constants_file_str).strip()
+    constants_file_str = re.sub(r"\s+", " ", constants_file_str).strip()
 
     # constant    "constant"
-    # \s+         one or more whitespaces 
+    # \s+         one or more whitespaces
     # [a-z]       identifiers must begin with a letter
     # [a-z_0-9]*  any letter, underscore or digit. zero or more times
-    # \s*         zero or more whitespaces 
+    # \s*         zero or more whitespaces
     # :           end delimiter (double colon)
-    names = re.findall(r'constant\s+([a-z][a-z_0-9]*)\s*:', constants_file_str, flags=re.IGNORECASE)
+    names = re.findall(
+        r"constant\s+([a-z][a-z_0-9]*)\s*:",
+        constants_file_str,
+        flags=re.IGNORECASE,
+    )
     print(names)
 
     # type        "type"
-    # \s+         one or more whitespaces 
+    # \s+         one or more whitespaces
     # [a-z]       identifiers must begin with a letter
     # [a-z_0-9]*  any letter, underscore or digit. zero or more times
     # \s+         one or more whitespaces
@@ -91,22 +103,31 @@ def get_constants_from_pkg(pkg_file_path):
     # \)          closing parenthesis (escaped)
     # \s*         zero or more whitespaces
     # ;           end delimiter (semicolon)
-    state_types = re.findall(r'type\s+([a-z][a-z_0-9]*)\s+is\s*\(\s*(.[^)]+)\s*\)\s*;', constants_file_str, flags=re.IGNORECASE)
-    print(state_types) # [('state_type', 'idle, calculation, finishing ')]  <- tuple access first element as state_types[0][0]
+    state_types = re.findall(
+        r"type\s+([a-z][a-z_0-9]*)\s+is\s*\(\s*(.[^)]+)\s*\)\s*;",
+        constants_file_str,
+        flags=re.IGNORECASE,
+    )
+    # [('state_type', 'idle, calculation, finishing ')]  <- tuple access first element as state_types[0][0]
+    print(state_types)
 
     # :           start delimiter (double colon)
     # \s*         zero or more whitespaces
     # (.*?)       capture group. any character zero or more times. lazy evaluation
     # \s*         zero or more whitespaces
     # :=          end delimiter (double colon and equal)
-    types = re.findall(r':\s*(.*?)\s*:=', constants_file_str, flags=re.IGNORECASE)
+    types = re.findall(
+        r":\s*(.*?)\s*:=", constants_file_str, flags=re.IGNORECASE
+    )
 
     # :=          start delimiter (double colon and equal)
     # \s*         zero or more whitespaces
     # (.*?)       capture group. any character zero or more times. lazy evaluation
     # \s*         zero or more whitespaces
     # ;           end delimiter (semicolon)
-    def_vals = re.findall(r':=\s*(.*?)\s*;', constants_file_str, flags=re.IGNORECASE)
+    def_vals = re.findall(
+        r":=\s*(.*?)\s*;", constants_file_str, flags=re.IGNORECASE
+    )
     return names, types, def_vals
 
 
@@ -121,15 +142,16 @@ def get_constants_in_file(file_path, constants_names):
     :rtype:                 list of str
     """
     constants_occur = set()
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         for line in f:
-            # remove VHDL comments (they could contain constants names) 
-            line = re.sub(r'(\s*--).*\n?', '', line, flags=re.MULTILINE)
+            # remove VHDL comments (they could contain constants names)
+            line = re.sub(r"(\s*--).*\n?", "", line, flags=re.MULTILINE)
             for n in constants_names:
                 # search for an exact match (constants names could partially contain other constants names)
-                if re.search(r'\b'+n+r'\b', line, re.IGNORECASE):
+                if re.search(r"\b" + n + r"\b", line, re.IGNORECASE):
                     constants_occur.add(n)
     return list(constants_occur)
+
 
 def build_dash_table_vhdl(table_content, header=[[]], prefix="-- "):
     """
@@ -148,11 +170,14 @@ def build_dash_table_vhdl(table_content, header=[[]], prefix="-- "):
     # now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     # heading = "--DASHTABLE. AUTO GENERATED @" + now + "\n"
     # add leading -- in every line of dash table to make the table a VHDL comment
-    dash_table_vhdl = [c if c != '\n' else "\n"+prefix for c in prefix+dash_table_]#heading+dash_table]
+    dash_table_vhdl = [
+        c if c != "\n" else "\n" + prefix for c in prefix + dash_table_
+    ]  # heading+dash_table]
     # make list of characters list of lines again
-    dash_table_vhdl = ''.join(dash_table_vhdl)
+    dash_table_vhdl = "".join(dash_table_vhdl)
     # print(dash_table_vhdl)
     return dash_table_vhdl
+
 
 def build_table_test(table_content):
     """
@@ -161,45 +186,55 @@ def build_table_test(table_content):
     return table_content
 
 
-
-
 def run(src_folder, def_pkg_path):
     """
     TODO
     src_folder = folder with VHDL files to go through
     def_pkg_path = constants def package file
     """
-    ## source files
-    vhdl_src_files = [src_folder+f for f in os.listdir(src_folder) if path.isfile(src_folder+f) and f.endswith(".vhd")]
+    # source files
+    vhdl_src_files = [
+        src_folder + f
+        for f in os.listdir(src_folder)
+        if path.isfile(src_folder + f) and f.endswith(".vhd")
+    ]
     # print(vhdl_src_files)
 
-    ## constants names, types and default values from def pkg
+    # constants names, types and default values from def pkg
     names, types, def_vals = get_constants_from_pkg(def_pkg_path)
 
     for vhdl_file in vhdl_src_files:
-        ## 1. read VHDL file, remove old table
-        with open(vhdl_file, 'r') as f:
+        # 1. read VHDL file, remove old table
+        with open(vhdl_file, "r") as f:
             vhdl_file_str = f.read()
         vhdl_file_str = remove_table(vhdl_file_str)
 
-        ## 2. list constants in the VHDL file 
+        # 2. list constants in the VHDL file
         constants_occur = get_constants_in_file(vhdl_file, names)
-        
-        ## 2.1 no constants used in this file, no need to build table
+
+        # 2.1 no constants used in this file, no need to build table
         if len(constants_occur) > 0:
-            
-            ## 3. build new dash table
-            header = [ ["Constant", "Type", "Default"] ]
-            constants_list = [ [n,t,d] for (n,t,d) in zip(names, types, def_vals) if n in constants_occur ]
+
+            # 3. build new dash table
+            header = [["Constant", "Type", "Default"]]
+            constants_list = [
+                [n, t, d]
+                for (n, t, d) in zip(names, types, def_vals)
+                if n in constants_occur
+            ]
             # dash_table_vhdl = build_dash_table_vhdl(constants_list, header, "-- ")
-            dash_table_vhdl = "-- +" + '\n-- '.join(map(str, build_table_test(constants_list))) + "---+" # debug
+            dash_table_vhdl = (
+                "-- +"
+                + "\n-- ".join(map(str, build_table_test(constants_list)))
+                + "---+"
+            )  # debug
             # print(dash_table_vhdl)
 
-            ## 4. add dash table to file str
+            # 4. add dash table to file str
             vhdl_file_str = add_table(vhdl_file_str, dash_table_vhdl)
-            #print(vhdl_file_str)
-        
-        ## 5. write back to VHDL file
-        with open(vhdl_file, 'w') as f:
+            # print(vhdl_file_str)
+
+        # 5. write back to VHDL file
+        with open(vhdl_file, "w") as f:
             f.write(vhdl_file_str)
         # print(vhdl_file_str)
---
+++ parser/vhdl.py
@@ -0,0 +1,184 @@
+import re
+
+
+def _get_raw_vhdl(file_path):
+    """
+    Removes all VHDL comments and substitutes all whitespaces/tabs/line breaks with a single whitespace
+    """
+    with open(file_path, "r") as f:
+        file_str = f.read()
+    # remove all VHDL comments
+    file_str = re.sub(r"(\s*--).*\n?", "", file_str, flags=re.MULTILINE)
+    # substitute all tabs, newlines and other whitespaces
+    file_str = re.sub(r"\s+", " ", file_str).strip()
+    return file_str
+
+
+def get_entity(file_path):
+    # [^- ]             anything that is NOT a dash or whitespace
+    # +                 one or more times
+    # \s*               zero or more whitespaces
+    # (                 begin of capture group---------------------------ENTITY
+    #     entity        "entity"
+    #     \s+           one or more whitespaces
+    #     .+            any character one or more times
+    #     \s+           one or more whitespaces
+    #     is            "is"
+    #     .*            any character zero or more times (->generics and ports)
+    #     end           "end"
+    #     \s+           one or more whitespaces
+    #     .*            any character zero or more times
+    #     ;             semicolon
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # architecture      "architecture"
+    m = re.search(
+        r"[^- ]+\s*(entity\s+.+\s+is.*end\s+.*;)\s*architecture",
+        _get_raw_vhdl(file_path),
+        flags=re.IGNORECASE,
+    )
+    entity_str = m.group(1)
+    # generics = get_entity_generics(entity_str)
+    # ports = get_entity_ports(entity_str)
+    # return generics, ports
+    return entity_str
+
+
+def get_entity_ports(entity_str):
+    # (                 begin of capture group---------------------ENTITY PORTS
+    #     port          "port"
+    #     \s*           zero or more whitespaces
+    #     \(            opening parenthesis
+    #     .*            any character zero or more times
+    #     \)            closing parenthesis
+    #     \s*           zero or more whitespaces
+    #     ;             semicolon
+    #     \s*           zero or more whitespaces
+    #     end           "end"
+    # )                 end of capture group
+    m = re.search(
+        r"(port\s*\(.*\)\s*;\s*end)", entity_str, flags=re.IGNORECASE
+    )
+    port_str = m.group(1)
+
+    # port variable names
+    # (                 begin of capture group----------------------------NAMES
+    #     [a-z]         lowercase letter (identifiers must begin with that)
+    #     [a-z_0-9]*    lowercase letter/underscore/digit. zero or more times
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # :                 double colon
+    port_names = re.findall(
+        r"([a-z][a-z_0-9]*)\s*:", port_str, flags=re.IGNORECASE
+    )
+
+    # port directions (in, out, inout)
+    # :                 double colon
+    # \s*               zero or more whitespaces
+    # (                 begin of capture group------------------------DIRECTION
+    #     [a-z]{2,}     lowercase letter. two or more times (shortest is "in")
+    # )                 end of capture group
+    # \s+               one or more whitespaces
+    port_dirs = re.findall(
+        r":\s*([a-z]{2,})\s+", port_str, flags=re.IGNORECASE
+    )
+
+    # port types (e.g. std_logic)
+    # :                 double colon
+    # \s*               zero or more whitespaces
+    # [a-z]{2,}         lowercase letter. two or more times
+    # \s+               one or more whitespaces
+    # (                 begin of capture group
+    #     .+?           any character one or more times. lazy evaluation
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # (?:               begin of non-capture group------------------------TYPES
+    #     ;             semicolon
+    #     |             OR
+    #     (?:           begin of non-capture group
+    #         \)        closing parenthesis
+    #         \s*       zero or more whitespaces
+    #         ;         semicolon
+    #         \s*       zero or more whitespaces
+    #         end       "end"
+    #     )             end of non-capture group
+    # )                 end of non-capture group
+    port_types = re.findall(
+        r":\s*[a-z]{2,}\s+(.+?)\s*(?:;|(?:\)\s*;\s*end))",
+        port_str,
+        flags=re.IGNORECASE,
+    )
+    # names, directions and ports as a list of tuples
+    ports = [(x, y, z) for x, y, z in zip(port_names, port_dirs, port_types)]
+    return ports
+
+
+def get_entity_generics(entity_str):
+    # (                 begin of capture group
+    #     generic       "generic"
+    #     \s*           zero or more whitespaces
+    #     \(            opening parenthesis
+    #     .*            any character zero or more times
+    #     \)            closing parenthesis
+    #     \s*           zero or more whitespaces
+    #     ;             semicolon
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # port              "port"
+    m = re.search(
+        r"(generic\s*\(.*\)\s*;)\s*port", entity_str, flags=re.IGNORECASE
+    )
+    if m is None:
+        return []
+    generic_str = m.group(1)
+
+    # generic variable names
+    # (                 begin of capture group----------------------------NAMES
+    #     [a-z]         lowercase letter (identifiers must begin with that)
+    #     [a-z_0-9]*    lowercase letter/underscore/digit. zero or more times
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # :                 double colon
+    # [^=]              any character that is not an equal sign
+    generic_names = re.findall(
+        r"([a-z][a-z_0-9]*)\s*:[^=]", generic_str, flags=re.IGNORECASE
+    )
+
+    # generic variable types
+    # :                 double colon
+    # \s*               zero or more whitespaces
+    # (                 begin of capture group----------------------------TYPES
+    #     .*?           any character zero or more times. lazy evaluation
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # :=                double colon and equal sign
+    generic_types = re.findall(
+        r":\s*(.*?)\s*:=", generic_str, flags=re.IGNORECASE
+    )
+
+    # generic variable default values
+    # :=                double colon and equal sign
+    # \s*               zero or more whitespaces
+    # (                 begin of capture group-------------------DEFAULT VALUES
+    #     .*?           any character zero or more times. lazy evaluation
+    # )                 end of capture group
+    # \s*               zero or more whitespaces
+    # (?:               begin of non-capture group
+    #     ;             semicolon
+    #     |             OR
+    #     (?:           begin of non-capture group
+    #         \)        closing parenthesis
+    #         \s*       zero or more whitespaces
+    #         ;         semicolon
+    #     )             end of non-capture group
+    # )                 end of non-capture group
+    generic_def_vals = re.findall(
+        r":=\s*(.*?)\s*(?:;|(?:\)\s*;))", generic_str, flags=re.IGNORECASE
+    )
+
+    # names, types and default values as a list of tuples
+    generics = [
+        (x, y, z)
+        for x, y, z in zip(generic_names, generic_types, generic_def_vals)
+    ]
+    return generics
