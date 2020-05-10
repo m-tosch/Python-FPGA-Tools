a454413aba27a948430448416f5b748a6ba08efa
mtosch <maximilian.tosch@rub.de>
2020-05-10 14:01:14 +0200

    unittest setup

---
+++ tests/test_vhdl.py
@@ -0,0 +1,26 @@
+import unittest
+
+# from regex_fun import vhdl
+from regex_fun import vhdl
+
+
+class TestVHDL(unittest.TestCase):
+    def setUp(self):
+        with open("./seven_segment_display.vhd", "r") as f:
+            self.buffer = f.read()
+            print(self.buffer)
+
+    # def tearDown(self):
+    #     pass
+
+    def test_something(self):
+        # arrange
+        # action
+        entity_str = vhdl.get_entity(self.buffer)
+        print(entity_str)
+        # assert
+        self.assertEqual(0, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
