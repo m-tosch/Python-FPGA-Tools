78e80b631e4fba2eb5834132d7ec0437c0aaf5d9
m-tosch <m-tosch@users.noreply.github.com>
2020-05-20 18:48:22 +0200

    sphinx configuration

---
+++ docs/source/conf.py
@@ -0,0 +1,70 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Regex Fun"
+copyright = "2020, Maximilian Tosch"
+author = "Maximilian Tosch"
+
+# The full version, including alpha/beta/rc tags
+release = "0.0.1"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+#
+# sphinxcontrib.napoleon - renders the docstrings correctly
+# m2r - allows to include .md markdown files in the html output
+# sphinx.ext.viewcode - includes a source code links in the doc to view it
+# sphinx.ext.githubpages - todo
+
+extensions = [
+    "sphinxcontrib.napoleon",
+    "m2r",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "classic"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+
+# stops sphinx to prefix module names in the doc
+# a method foo() in module bar then appears as just foo() instead of bar.foo()
+add_module_names = False
