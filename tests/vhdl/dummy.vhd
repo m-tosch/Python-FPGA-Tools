f19c9fa0850400736a4c15684a6fb47a8890ac7a
m-tosch <m-tosch@users.noreply.github.com>
2020-05-24 16:04:31 +0200

    fix gitignore

---
+++ tests/vhdl/dummy.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity dummy is
+-- no generics
+-- no ports
+end dummy;
+
+architecture behavioral of dummy is
+-- no signals
+begin
+
+-- no process
+
+end behavioral;
