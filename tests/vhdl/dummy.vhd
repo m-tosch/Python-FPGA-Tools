51bbecbafc28bf43b0120beb4a86b99a82051770
m-tosch <m-tosch@users.noreply.github.com>
2020-05-26 19:42:28 +0200

    add entity for now

--- tests/vhdl/dummy.vhd
+++ tests/vhdl/dummy.vhd
@@ -1,13 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- no entity
+entity dummy is
   -- no generics
   -- no ports
-
+end dummy;
 
 -- no architecture
 
   -- no signals
 
   -- no process
+
+
